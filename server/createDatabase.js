3e10b5a53d835547784ce2eb0e67a3bea2a484e8
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-07-05 18:47:58 +0100

    Create database and table

---
+++ server/createDatabase.js
@@ -0,0 +1,44 @@
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const hostName = process.env.DB_HOST;
+const userName = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+const tableName = process.env.DB_TABLE_NAME;
+
+//create DB
+
+const connection = mysql.createConnection({
+    host: hostName,
+    user: userName,
+    password: password,
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected');
+
+    const createDB = `CREATE DATABASE ${dbName}`;
+
+    connection.query(createDB, (err) => {
+        if (err) throw err;
+        console.log("Database created");
+
+        connection.query(`USE ${dbName}`, function (err) {
+            if (err) throw err;
+            console.log("Table Used");
+        });
+
+        const createTable = `CREATE TABLE ${tableName} (id INT NOT NULL AUTO_INCREMENT, firstName VARCHAR(20) NOT NULL, surname VARCHAR(40) NOT NULL, email VARCHAR(40) NOT NULL, telephone VARCHAR(40) NOT NULL, gender VARCHAR(10), dateOfBirth DATE, comments VARCHAR(255) NOT NULL, PRIMARY KEY (id));`;
+
+        connection.query(createTable, function (err) {
+            if (err) throw err;
+            console.log("Table created");
+        });
+
+        connection.end();
+    });
+});
